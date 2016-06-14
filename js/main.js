e79e4cb316629b44f56336ba6998088549e24c68
Jordan Gray <jgray@organic.com>
2016-06-14 09:20:19 -0700

    remove drag and drop, add dat.gui
    sliders to position the model before making a gif, easier to manipulate
    than camera orbit controls

--- js/main.js
+++ js/main.js
@@ -230,18 +230,6 @@ function animate(){
 	render();
 }
 
-/***
-* DRAG N DERP FUNCTIONS
-***/
-function fileSupportTest(){
-	// Check for the various File API support.
-	if (window.File && window.FileReader && window.FileList && window.Blob) {
-	  // Great success! All the File APIs are supported.
-	} else {
-	  alert('The File APIs are not fully supported in this browser.');
-	}
-}
-
 function renderNewModel(files){
 	var f = files[0];
 	//TODO STL ONLY
